9bc2f7f969741122a0bb2d9a2760804463073022
Yury Zhukovsky <zhukovsky.yuri@gmail.com>
2025-01-29 19:38:45 +0300

    Fix - Error: The process '/opt/hostedtoolcache/Python/3.10.16/x64/bin/flake8' failed with exit code 1

--- homework/yuri_zhukovsky/hw7/hw7_2.py
+++ homework/yuri_zhukovsky/hw7/hw7_2.py
@@ -1,7 +1,9 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
+
 def display_item(key, value):
     print(key * value)
+
 
 for item in words.items():
     display_item(*item)
--- homework/yuri_zhukovsky/hw7/hw7_3.py
+++ homework/yuri_zhukovsky/hw7/hw7_3.py
@@ -3,6 +3,7 @@ ro_2 = 'результат операции: 54'
 rrp = 'результат работы программы: 209'
 rez = 'результат: 2'
 
+
 def process_res(*results):
     values = []
 
@@ -12,5 +13,6 @@ def process_res(*results):
 
     for value in values:
         print(value + 10)
+
 
 process_res(ro_1, ro_2, rrp, rez)
