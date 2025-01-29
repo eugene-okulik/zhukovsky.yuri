26f6ac15aca32df89acb87757d1dd7188180fb30
Yury Zhukovsky <zhukovsky.yuri@gmail.com>
2025-01-29 19:32:12 +0300

    Homework_7 done

---
+++ homework/yuri_zhukovsky/hw7/hw7_3.py
@@ -0,0 +1,16 @@
+ro_1 = 'результат операции: 42'
+ro_2 = 'результат операции: 54'
+rrp = 'результат работы программы: 209'
+rez = 'результат: 2'
+
+def process_res(*results):
+    values = []
+
+    for result in results:
+        value = int(result.split(': ')[1])
+        values.append(value)
+
+    for value in values:
+        print(value + 10)
+
+process_res(ro_1, ro_2, rrp, rez)
