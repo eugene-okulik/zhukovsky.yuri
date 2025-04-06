306c73ab0913e048d7f7847f916be3cbd5889d99
Yuri Zhukovsky <zhukovsky.yuri@gmail.com>
2025-04-07 00:22:07 +0300

    start to do hw15

---
+++ homework/yuri_zhukovsky/hw15/hw15_1.py
@@ -0,0 +1,19 @@
+import mysql.connector as mysql
+
+db = mysql.connect(
+    user='st-onl',
+    passwd='AVNS_tegPDkI5BlB2lW5eASC',
+    host='db-mysql-fra1-09136-do-user-7651996-0.b.db.ondigitalocean.com',
+    port=25060,
+    database='st-onl'
+)
+
+cursor = db.cursor(dictionary=True)
+cursor.execute('SELECT * FROM students')
+data = cursor.fetchall()
+print(data)
+
+
+
+
+db.close()
