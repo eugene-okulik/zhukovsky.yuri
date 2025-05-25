a054906664f33d77583107938f6d1d321a7578ba
Yuri Zhukovsky <zhukovsky.yuri@gmail.com>
2025-05-25 17:52:12 +0300

    homework_18 fix flake8

--- homework/yuri_zhukovsky/hw18/hw18_1.py
+++ homework/yuri_zhukovsky/hw18/hw18_1.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def post_an_object():
     body = {
         "name": "yzhtest",
@@ -22,6 +23,7 @@ def post_an_object():
     assert response.json()['data']['color'] == 'dark black', 'Color is incorrect'
     assert  response.json()['data']['size'] == 'medium', 'Size is incorrect'
 
+
 def new_object():
     body = {
         "name": "yzhtest",
@@ -39,8 +41,10 @@ def new_object():
     # print(response.json()['id'])
     return response.json()['id']
 
+
 def clear_an_object(obj_id):
     requests.delete(f'http://167.172.172.115:52353/object/{obj_id}')
+
 
 def put_an_object():
     obj_id = new_object()
@@ -66,6 +70,7 @@ def put_an_object():
     assert response.json()['data']['color'] == 'white', 'Color is incorrect'
     assert  response.json()['data']['size'] == 'big', 'Size is incorrect'
 
+
 def patch_an_object():
     obj_id = new_object()
     body = {
@@ -86,6 +91,7 @@ def patch_an_object():
     assert response.json()['data']['color'] == 'dark black', 'Color is incorrect'
     assert  response.json()['data']['size'] == 'medium', 'Size is incorrect'
 
+
 def delete_an_object():
     obj_id = new_object()
     response = requests.delete(f'http://167.172.172.115:52353/object/{obj_id}')
@@ -94,6 +100,7 @@ def delete_an_object():
     assert response.status_code == 200, 'Status code is incorrect'
     assert response.content.decode(
         'utf-8') == f'Object with id {obj_id} successfully deleted', 'Message is incorrect'
+
 
 post_an_object()
 put_an_object()
